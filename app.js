9e11d39633d7a66b3af3988921cde07840da73eb
ahmedhajjo <ahmad_1996@live.com>
2019-08-03 20:57:01 +0400

    Changes

--- app.js
+++ app.js
@@ -7,67 +7,76 @@
 // var express = require('express');
 // var app = express();
 // var http = require('http').Server(app);
-var io = require('socket.io')(process.env.PORT || 80); 
+var io = require('socket.io')(process.env.PORT || 80);
 var mongoose = require('mongoose');
 
 var uuid = require('uuid');
 var shortId = require('shortid');
 var players = [];
 
-console.log('Hello world');
-
-
 
 
 io.on('connection',function(socket){
     var thisClient = shortId.generate();
-    players.push(thisClient);
-    console.log('client connected , brodcasting spawn , id:', thisClient); 
-    socket.broadcast.emit('spawn', {id: thisClient});
-    
-    
-     players.forEach(function(PlayerId){
+
+    var player = {
+        id: thisClient,
+        
+            x :0,
+            y:0,
+            z:0
+
+    };
+
+    players[thisClient] = player;
+    console.log('client connected , brodcasting spawn , id:', thisClient);
+    socket.broadcast.emit('spawn', players[PlayerId]);
+    socket.broadcast.emit('requestPos');
+
+
+
+    for(var PlayerId in players){
 
         if(PlayerId == thisClient)
-        return;
-
+        continue;
         socket.emit('spawn', {id:PlayerId});
         console.log('sending spawn to new player', PlayerId);
-     });
-
-     
-
+     };
 
     socket.on('move', function(data){
         data.id = thisClient;
         console.log('client moved',JSON.stringify(data));
-     
+        player.x = data.x;
+        player.y = data.y;
+        player.z = data.z;
         socket.broadcast.emit('move', data);
-        
-    }); 
+
+    });
+
+    socket.on('UpdatePosition', function(data){
+            console.log('update Position :', data);
+            data.id =thisClient;
+            socket.broadcast.emit('UpdatePosition', data)
+    });
 
     socket.on('disconnect', function()
     {
         console.log('ClientDisconnected');
-        players.splice(players.indexOf(thisClient),1);
+        delete players[thisClient];
         socket.broadcast.emit('disconnected', {id: thisClient});
 
     });
 
-    // app.listen(process.env.PORT || 80,function(){
-    //     console.log('listening on 80')
-    // });
-    // console.log("---------------Running Server----------------"); 
-}) 
+   
+ })
 
-mongoose.connect('mongodb://localhost/mongoose_basics', function (err) {
- 
-   if (err) throw err;
- 
-   console.log('Successfully connected');
- 
-});
+// mongoose.connect('mongodb://localhost/mongoose_basics', function (err) {
+
+//    if (err) throw err;
+
+//    console.log('Successfully connected');
+
+// });
 
 
 
- 
